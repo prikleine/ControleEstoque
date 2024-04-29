4076b55062e311d7afdc1f56745eb022743bd233
suersil <suelenregsilva@gmail.com>
2024-04-29 11:42:42 -0300

    ItemEstoqueNotFoundException

--- src/main/java/ada/tech/controle/estoque/exception/ItemEstoqueNotFoundException.java
+++ src/main/java/ada/tech/controle/estoque/exception/ItemEstoqueNotFoundException.java
@@ -3,9 +3,8 @@ package ada.tech.controle.estoque.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-public class ItemEstoqueNotFoundException {
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public static class ItemNotFoundException extends RuntimeException {
-    }
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ItemEstoqueNotFoundException extends RuntimeException {
+
 }
 
